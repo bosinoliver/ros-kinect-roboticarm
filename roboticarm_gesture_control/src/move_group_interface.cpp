c7acedf7d5ff2e21359369b1dd9572a6ab525003
bosinoliver <oliver.bosin@unibw.de>
2019-04-30 12:29:39 +0200

    update

--- roboticarm_gesture_control/src/move_group_interface.cpp
+++ roboticarm_gesture_control/src/move_group_interface.cpp
@@ -92,9 +92,7 @@
 
 
 
-int main(int argc, char** argv)
-
-{
+int main(int argc, char** argv){
 
   ros::init(argc, argv, "move_group_interface");
 
@@ -128,17 +126,40 @@ int main(int argc, char** argv)
 	move_group_arm.setPoseReferenceFrame("base_link");
 
 	move_group_arm.setGoalTolerance(0.005);
-	move_group_arm.setPlanningTime(20);
+	move_group_arm.setPlanningTime(5);
 
 	move_group_gripper.clearPoseTargets();
 	move_group_gripper.setStartStateToCurrentState();
 	move_group_gripper.setPoseReferenceFrame("base_link");
 
 	move_group_gripper.setGoalTolerance(0.005);
-	move_group_gripper.setPlanningTime(20);
+	move_group_gripper.setPlanningTime(5);
 
 
+	/* Here a TransformListener object is created.
+		 * Once the listener is created it starts receiving tf2 transformations over the wire
+		 * and buffers them for up to 10 seconds*/
+	  tf2_ros::Buffer tfBuffer;
+	  tf2_ros::TransformListener tfListener(tfBuffer);
 
+	  ros::Rate rate(10.0);
+	  while (node_handle.ok()){
+	    geometry_msgs::TransformStamped transformStamped_left_hand_1;
+	    geometry_msgs::TransformStamped transformStamped_right_hand_1;
+	    try{
+			/*here the transform between openni_link and left_hand_1 is calculated
+			 *openni_link -> kinect , left_hand_1 -> right hand of person*/
+	      transformStamped_left_hand_1 = tfBuffer.lookupTransform("torso_1", "left_hand_1",
+	                               ros::Time(0));
+	      transformStamped_right_hand_1 = tfBuffer.lookupTransform("right_shoulder_1", "right_hand_1",
+	      	                               ros::Time(0));
+	    }
+	    catch (tf2::TransformException &ex) {
+	      ROS_WARN("%s",ex.what());
+
+	      ros::Duration(1.0).sleep();
+	      continue;
+	    }
 
 
 
@@ -173,13 +194,20 @@ int main(int argc, char** argv)
   moveit::core::RobotStatePtr current_state;
   std::vector<double> joint_group_positions;
 
-  move_group_arm.setPositionTarget(0.0, 0.317388, 0.231109);
+  move_group_arm.setPositionTarget(transformStamped_left_hand_1.transform.translation.x , transformStamped_left_hand_1.transform.translation.z*(-1), transformStamped_left_hand_1.transform.translation.y);
+  ROS_INFO_NAMED("planning","x : %lf y: %lf z: %lf",transformStamped_left_hand_1.transform.translation.x, transformStamped_left_hand_1.transform.translation.z*(-1), transformStamped_left_hand_1.transform.translation.y );
+
   bool succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
   if(succes){
+	  succes = false;
 	  joint_group_positions = my_plan.trajectory_.joint_trajectory.points.back().positions;
-	  joint_group_positions[4] = -0.385;
+	  joint_group_positions[4] = -0.0;
 	  move_group_arm.setJointValueTarget(joint_group_positions);
-	  move_group_arm.move();
+	  succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+	  if(succes){
+	  move_group_arm.execute(my_plan);
+	  }
   }
 
 
@@ -192,20 +220,19 @@ int main(int argc, char** argv)
 
 
 
-  move_group_arm.move();
+
 
   if(true){
 
-	  joint_group_positions[0] = -0.300;
+	  //joint_group_positions[0] = -0.300;
 
-	  move_group_gripper.setJointValueTarget(joint_group_positions);
+	  //move_group_gripper.setJointValueTarget(joint_group_positions);
 
-	  move_group_gripper.move();
+	  //move_group_gripper.move();
   }
 
-
-  ros::shutdown();
-
+  rate.sleep();
+	  }
   return 0;
 
 }
