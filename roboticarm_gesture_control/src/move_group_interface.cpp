818c60630eea1be8c8a5570b78ecc3d5e767b0c6
bosinoliver <oliver.bosin@unibw.de>
2019-05-13 12:09:27 +0200

    update

--- roboticarm_gesture_control/src/move_group_interface.cpp
+++ roboticarm_gesture_control/src/move_group_interface.cpp
@@ -69,12 +69,12 @@
 /* Author: Oliver Bosin*/
 
 
-
+#include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
-
+#include <tf2/LinearMath/Vector3.h>
 
 #include <moveit_msgs/DisplayRobotState.h>
 
@@ -85,9 +85,6 @@
 #include <moveit_msgs/AttachedCollisionObject.h>
 
 #include <moveit_msgs/CollisionObject.h>
-
-
-
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 
@@ -97,6 +94,12 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "move_group_interface");
 
   ros::NodeHandle node_handle;
+
+  tf2::Vector3 old_torso_to_hand;
+  old_torso_to_hand[0] = 0.0;
+  old_torso_to_hand[1] = 0.0;
+  old_torso_to_hand[2] = 0.0;
+  tf2::Vector3 new_torso_to_hand;
 
   ros::AsyncSpinner spinner(1);
 
@@ -126,29 +129,82 @@ int main(int argc, char** argv){
 	move_group_arm.setPoseReferenceFrame("base_link");
 
 	move_group_arm.setGoalTolerance(0.005);
-	move_group_arm.setPlanningTime(5);
+	move_group_arm.setPlanningTime(2);
 
 	move_group_gripper.clearPoseTargets();
 	move_group_gripper.setStartStateToCurrentState();
 	move_group_gripper.setPoseReferenceFrame("base_link");
 
 	move_group_gripper.setGoalTolerance(0.005);
-	move_group_gripper.setPlanningTime(5);
+	move_group_gripper.setPlanningTime(2);
 
+	tf2_ros::Buffer tfBuffer;
 
 	/* Here a TransformListener object is created.
-		 * Once the listener is created it starts receiving tf2 transformations over the wire
-		 * and buffers them for up to 10 seconds*/
-	  tf2_ros::Buffer tfBuffer;
-	  tf2_ros::TransformListener tfListener(tfBuffer);
+			 * Once the listener is created it starts receiving tf2 transformations over the wire
+			 * and buffers them for up to 10 seconds*/
 
-	  ros::Rate rate(10.0);
-	  while (node_handle.ok()){
-	    geometry_msgs::TransformStamped transformStamped_left_hand_1;
-	    geometry_msgs::TransformStamped transformStamped_right_hand_1;
-	    try{
-			/*here the transform between openni_link and left_hand_1 is calculated
-			 *openni_link -> kinect , left_hand_1 -> right hand of person*/
+	tf2_ros::TransformListener tfListener(tfBuffer);
+
+    geometry_msgs::TransformStamped transformStamped_left_hand_1;
+	geometry_msgs::TransformStamped transformStamped_right_hand_1;
+	geometry_msgs::TransformStamped transformStamped_shoulder_to_elbow;
+    geometry_msgs::TransformStamped transformStamped_elbow_to_hand;
+
+	/*get transform from shoulder to elbow and elbow to hand for armlength*/
+    while (node_handle.ok()){
+
+    	    try{
+
+    	    	transformStamped_shoulder_to_elbow = tfBuffer.lookupTransform("left_shoulder_1", "left_elbow_1",
+    	    			                               ros::Time(0));
+
+    	    	transformStamped_elbow_to_hand = tfBuffer.lookupTransform("left_elbow_1","left_hand_1",
+    	    			                               ros::Time(0));
+    	    }
+    	    catch (tf2::TransformException &ex) {
+    	      ROS_WARN("%s",ex.what());
+
+
+    	      continue;
+    	    }
+    	    break;
+    }
+
+    /*convert from geometry_msgs::Vector3 to tf2::Vector3 so the use of the length() function is possible*/
+
+    tf2::Vector3 shoulder_to_elbow_vector;
+	tf2::Vector3 elbow_to_hand_vector;
+
+	shoulder_to_elbow_vector[0] =  transformStamped_shoulder_to_elbow.transform.translation.x;
+	shoulder_to_elbow_vector[1] =  transformStamped_shoulder_to_elbow.transform.translation.y;
+	shoulder_to_elbow_vector[2] =  transformStamped_shoulder_to_elbow.transform.translation.z;
+
+	elbow_to_hand_vector[0] =  transformStamped_elbow_to_hand.transform.translation.x;
+	elbow_to_hand_vector[1] =  transformStamped_elbow_to_hand.transform.translation.y;
+	elbow_to_hand_vector[2] =  transformStamped_elbow_to_hand.transform.translation.z;
+
+	/*calculate armlength of user*/
+
+	double armlength = shoulder_to_elbow_vector.length() + elbow_to_hand_vector.length();
+
+	/*calculate conversion factor for position target*/
+
+	double conversion_factor = 0.40 / armlength;
+
+
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	moveit::core::RobotStatePtr current_state;
+	std::vector<double> joint_group_positions;
+
+	ros::Rate rate(20.0);
+
+	/*main loop*/
+
+	while (node_handle.ok()){
+
+		try{
+
 	      transformStamped_left_hand_1 = tfBuffer.lookupTransform("torso_1", "left_hand_1",
 	                               ros::Time(0));
 	      transformStamped_right_hand_1 = tfBuffer.lookupTransform("right_shoulder_1", "right_hand_1",
@@ -157,60 +213,50 @@ int main(int argc, char** argv){
 	    catch (tf2::TransformException &ex) {
 	      ROS_WARN("%s",ex.what());
 
-	      ros::Duration(1.0).sleep();
+
 	      continue;
 	    }
 
 
 
-  // Getting Basic Information
-
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
-
-  //
-
-  // We can print the name of the reference frame for this robot.
-
-  //ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group_arm.getPlanningFrame().c_str());
-  //ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group_gripper.getPlanningFrame().c_str());
+	    new_torso_to_hand[0] = transformStamped_left_hand_1.transform.translation.x;
+	    new_torso_to_hand[1] = transformStamped_left_hand_1.transform.translation.y;
+	    new_torso_to_hand[2] = transformStamped_left_hand_1.transform.translation.z;
 
 
-  // We can also print the name of the end-effector link for this group.
-
-  //ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_arm.getEndEffectorLink().c_str());
-  //ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group_gripper.getEndEffectorLink().c_str());
-
-
-
-  // Planning to a Pose goal
+  // Planning to a position target
 
   // ^^^^^^^^^^^^^^^^^^^^^^^
 
-  // We can plan a motion for this group to a desired pose for the
+  // We can plan a motion for this group to a desired position target for the
 
   // end-effector.
  
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-  moveit::core::RobotStatePtr current_state;
-  std::vector<double> joint_group_positions;
 
-  move_group_arm.setPositionTarget(transformStamped_left_hand_1.transform.translation.x , transformStamped_left_hand_1.transform.translation.z*(-1), transformStamped_left_hand_1.transform.translation.y);
-  ROS_INFO_NAMED("planning","x : %lf y: %lf z: %lf",transformStamped_left_hand_1.transform.translation.x, transformStamped_left_hand_1.transform.translation.z*(-1), transformStamped_left_hand_1.transform.translation.y );
 
-  bool succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
 
-  if(succes){
-	  succes = false;
-	  joint_group_positions = my_plan.trajectory_.joint_trajectory.points.back().positions;
-	  joint_group_positions[4] = -0.0;
-	  move_group_arm.setJointValueTarget(joint_group_positions);
-	  succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+  if((old_torso_to_hand.distance(new_torso_to_hand)) > 0.025){
+
+	  move_group_arm.setPositionTarget(conversion_factor * transformStamped_left_hand_1.transform.translation.x - 0.12, conversion_factor * transformStamped_left_hand_1.transform.translation.z*(-1), conversion_factor * transformStamped_left_hand_1.transform.translation.y);
+
+
+	  ROS_INFO_NAMED("planning","x : %lf y: %lf z: %lf",conversion_factor * transformStamped_left_hand_1.transform.translation.x, conversion_factor * transformStamped_left_hand_1.transform.translation.z*(-1), conversion_factor * transformStamped_left_hand_1.transform.translation.y );
+	  ROS_INFO_NAMED("distance","distance to target: %lf", old_torso_to_hand.distance(new_torso_to_hand));
+
+	  bool succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
 	  if(succes){
-	  move_group_arm.execute(my_plan);
+		  succes = false;
+		  joint_group_positions = my_plan.trajectory_.joint_trajectory.points.back().positions;
+		  joint_group_positions[4] = -0.0;
+		  move_group_arm.setJointValueTarget(joint_group_positions);
+		  succes = (move_group_arm.plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
+		  if(succes){
+			  move_group_arm.execute(my_plan);
+			  old_torso_to_hand = new_torso_to_hand;
+		  }
 	  }
   }
-
-
 
 
 
